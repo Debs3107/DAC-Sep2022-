54d01cce85f8eb413e19e974dc5432ff013c596e
Debs3107 <88672521+Debs3107@users.noreply.github.com>
2022-09-20 17:58:32 +0530

    Add files via upload
    Assignment 2

---
+++ A10.java
@@ -0,0 +1,25 @@
+class A10	
+{
+public static void main(String args[])
+{
+	int a=65;
+for(int i=4;i>=0;i--)
+{
+	for(int j=0;j<i;j++)
+
+	{
+		System.out.print(" ");
+	}
+	for(int k=i;k<=4;k++)
+		
+	{
+	
+		
+		System.out.print((char)(a+k)+" ");		
+		
+	}
+	System.out.println();
+
+}
+}
+}
---
+++ A6.java
@@ -0,0 +1,15 @@
+class A6{
+public static void main(String args[]){
+for(int i=1;i<=5;i++){
+for(int j=1;j<=2*(5-i);j++){
+System.out.print(" ");
+}
+for(int k=5-i+1;k<5;k++){
+
+System.out.print(" *");
+}
+System.out.println();
+}
+
+}
+}
